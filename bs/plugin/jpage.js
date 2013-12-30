b3068052dff747b88b3045e70d83b9d9d2f9baa9
hikaMaeng <hika00@gmail.com>
2013-12-30 12:30:15 +0900

    jpage update

--- bs/plugin/jpage.js
+++ bs/plugin/jpage.js
@@ -1,7 +1,7 @@
 bs.$register( 'method', 'jpage', (function(){
 	var jp, jpage, r0, r1, r2, s, e;
 	jp = function( $v ){this.v = $v},
-	r0 = /["]/g, r1 = /\r\n/g, r2 = /\n/g, s = '<%', e = '%>',
+	r0 = /["]/g, r1 = /\\/g, r2 = /\r\n|\r|\n/g, s = '<%', e = '%>',
 	jpage = function( $str, $data, $render, $end ){
 		var t0, t1, i, j, k, v, $$R;
 		if( $str instanceof jp ) v = $str.v
@@ -11,10 +11,10 @@ bs.$register( 'method', 'jpage', (function(){
 			while( i < j ){
 				t0 = $str[i++];
 				if( ( k = t0.indexOf( e ) ) > -1 ) t1 = t0.substring( 0, k ), t0 = t0.substr( k + 2 ), v += t1.charAt(0) == '=' ? '$$R( ' + t1.substr(1) + ' );' : t1;
-				v += '$$R( "' + t0.replace( r0, '\\"' ).replace( r1, '\\n' ).replace( r2, '\\n' ) + '");';
+				v += '$$R( "' + t0.replace( r0, '\\"' ).replace( r1, '\\\\' ).replace( r2, '\\n' ) + '");';
 			}
 		}
-		new Function( '$$R, $data', v )( $render, $data );
+		if( $render ) new Function( '$$R, $data', v )( $render, $data );
 		if( $end ) $end( v );
 		return new jp(v);
 	};
