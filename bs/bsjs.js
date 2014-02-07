1e19ea45d6662cfe447fb46c7987dbef41280530
hikaMaeng <hika00@gmail.com>
2014-02-07 19:20:19 +0900

    innerHTML수정

--- bs/bsjs.js
+++ bs/bsjs.js
@@ -613,44 +613,38 @@ function DOM(){
 			};
 		})(doc),
 		html = (function(doc){
-			var t0, t1, t2, t3, t4, t5,
-				i, j,
-				n0, n1, n2,
-				div, tbody, tags, parent;
-			div = doc.createElement( 'div' );
-			tbody = doc.createElement( 'tbody' );
+			var t0, i, div, tbody, tags;
+			div = doc.createElement('div'),
+			tbody = doc.createElement('tbody'),
 			tags = {
-				tr :	 [ 1, '<table><tbody>', '</tbody></table>' ],
-				col : [ 1, '<table><tbody></tbody><colgroup>', '</colgroup></table>' ],
-				th : [ 2, '<table><tbody><tr>', '</tr></tbody></table>' ],
-				td : [ 2, '<table><tbody><tr>', '</tr></tbody></table>' ],
-				option : [ 0, '<select>', '</select>' ]
-			};
-			t0 = 'thead,tfoot,tbody,caption,colgroup'.split(','),i = t0.length;
-			while(i--) tags[t0[i]] = [0,'<table>','</table>'];
-			return html = function( str, target, mode ) {
-				t0 = str.replace( trim, '' ),
-				n0 = t0.indexOf(' '),
-				n1 = t0.indexOf('>'),
-				n2 = t0.indexOf('/');
-				t1 = ( n0 != -1 && n0 < n1 ) ? t0.substring( 1, n0 ) : ( n2 != -1 && n2 < n1 ) ? t0.substring( 1, n2 ) : t0.substring( 1, n1 );
+				tr:[1, '<table><tbody>', '</tbody></table>'],
+				col:[1, '<table><tbody></tbody><colgroup>', '</colgroup></table>'],
+				th:[2, '<table><tbody><tr>', '</tr></tbody></table>'],
+				option:[0, '<select>', '</select>']
+			},
+			tags.td = tags.th,
+			t0 = 'thead,tfoot,tbody,caption,colgroup'.split(','), i = t0.length;
+			while( i-- ) tags[t0[i]] = [0,'<table>','</table>'];
+			return function( str, target, mode ){
+				var t0, t1, t2, t3, t4, t5, i, j, n0, n1, n2, parent;
+				t0 = str.replace( trim, '' ), n0 = t0.indexOf(' '), n1 = t0.indexOf('>'), n2 = t0.indexOf('/'),
+				t1 = ( n0 != -1 && n0 < n1 ) ? t0.substring( 1, n0 ) : ( n2 != -1 && n2 < n1 ) ? t0.substring( 1, n2 ) : t0.substring( 1, n1 ),
 				t1 = t1.toLowerCase();
 				if( mode == 'html' && target.nodeName.toLowerCase() == 'table' && t1 == 'tr' ) t1 = 'tbody';
-				if( mode == '>' || 'html+' && t1 == 'tr' ) target = target.getElementsByTagName("tbody")[0] || target.appendChild(tbody), target.getElementsByTagName("tbody")[0];
-				if( tags[t1] ) {
-					div.innerHTML = tags[t1][1] + str + tags[t1][2];
-					t2 = div.childNodes[0];
-					if( tags[t1][0] ) for( i = 0; i < tags[t1][0]; i++ ) t2 = t2.childNodes[0];
+				if( mode == '>' || 'html+' && t1 == 'tr' ) target = target.getElementsByTagName('tbody')[0] || ( target.appendChild(tbody), target.getElementsByTagName('tbody')[0] );
+				if( tags[t1] ){
+					div.innerHTML = tags[t1][1] + str + tags[t1][2], t2 = div.childNodes[0];
+					if( tags[t1][0] ) for( i = 0 ; i < tags[t1][0] ; i++ ) t2 = t2.childNodes[0];
 					parent = t2;
 				}else div.innerHTML = str, parent = div;
 				i = parent.childNodes.length;
-				if( !target ) {target = {length : 0};while( i-- ) target[target.length++] = parent.childNodes[i];Array.prototype.reverse.call(target);}
-				else if( mode == 'html' ) {
-					j = target.childNodes.length;
-					if( j > 0 ) while( j-- ) target.removeChild(target.childNodes[0]);
+				if( !target ){
+					target = {length : 0};
+					while( i-- ) target[target.length++] = parent.childNodes[i];Array.prototype.reverse.call(target);
+				}else if( mode == 'html' ){
+					bs.Dom(target.childNodes).S(null);
 					while( i-- ) target.appendChild(parent.childNodes[0]);
-				}
-				else if( mode == '+html' ) while( i-- ) target.insertBefore(parent.childNodes[parent.childNodes.length - 1], target.childNodes[0]);
+				}else if( mode == '+html' ) while( i-- ) target.insertBefore( parent.childNodes[parent.childNodes.length - 1], target.childNodes[0] );
 				else while( i-- ) target.appendChild(parent.childNodes[0]);
 				return target;
 			};
